411ba47eaf34f58a40033c438bbc9d21ecd2c5a7
Ben Jaeger <benjaeger99@gmail.com>
2022-06-22 05:13:19 +0100

    FEAT: Implement getting type name to index map

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
 import os
 
-import pokebase
+import pokebase as pb
 from requests.exceptions import HTTPError
 import cv2
+
+
+def get_type_indexes():
+    types = dict()
+    i = 1
+    while True:
+        try:
+            types[pb.type_(i).name] = i
+            i += 1
+        except HTTPError:
+            break
+    return types
 
 
 def main():
@@ -10,4 +22,4 @@ def main():
 
 
 if __name__ == '__main__':
-    main()
+    print(get_type_indexes())
