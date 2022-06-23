bec957010d657e7eb59fa14b84f7f61c73661fc0
Ben Jaeger <benjaeger99@gmail.com>
2022-06-23 10:04:20 +0100

    FEAT Implement scraping data for every pokemon until no endpoint found

--- main.py
+++ main.py
@@ -30,10 +30,38 @@ def get_generation_indexes():
             break
     return generations
 
+
 def main():
-    pass
+    # get index maps for types and generations
+    type_map = get_type_indexes()
+    gen_map = get_generation_indexes()
+
+    # write the header
+    with open("data.txt", "w") as f:
+        f.write("name,type1,type2,gen,hp,att,def,spatt,spdef,speed,hght,wght\n")
+
+    # loop over pokemon until an error is raised from the endpoint not being found
+    index = 1
+    data = {}
+    while True:
+        try:
+            pokemon = pb.pokemon(index)
+
+            # get types and pad with empty entry if only one exists
+            types = ([pokemon.types[n].type.name for n in range(len(pokemon.types))] + [""])[:2]
+            type1, type2 = sorted([type_map[t] for t in types])
+
+            gen = gen_map[pokemon.name]
+            hp, att, def_, spatt, spdef, speed = [pokemon.stats[n].base_stat for n in range(6)]
+            height, weight = [pokemon.height, pokemon.weight]
+
+            data[index] = [type1, type2, gen, hp, att, def_, spatt, spdef, speed, height, weight]
+            # print(type1, type2, gen, hp, att, def_, spatt, spdef, speed, height, weight)
+        except HTTPError:  # if the pokemon is not found, break the loop
+            break
+        index += 1
 
 
 if __name__ == '__main__':
-    print(len(get_generation_indexes()))
+    main()
 
